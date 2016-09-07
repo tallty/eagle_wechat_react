ce26aaca1a39a4ae29e5220a01ec47f94255c63b
zhangliugang <www.zhangyi508@gmail.com>
2016-09-07 13:29:29 +0800

    add eagle wechat

---
+++ src/components/App.jsx
@@ -0,0 +1,58 @@
+import React, { Component, PropTypes } from 'react'
+import { Row, Col } from 'antd'
+import classnames from 'classnames';
+import styles from './App.less';
+import { Time } from './time/Time';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    let tab_height = document.body.clientHeight
+    return (
+      <div className={styles.App_content} style={{height: tab_height, overflow: "auto"}}>
+        <Row className={styles.header}>
+          <Col span={4} className={styles.logo_pic_content}>
+            <img src="src/images/logo.png" alt="" className={styles.logo_pic} />智慧气象服务云平台
+          </Col>
+          <Col span={6} className={styles.amount} >59,235,594</Col>
+          <Col span={5} className={styles.today_amount}><img src="src/images/up_icon.svg" alt="" className={styles.up_icon} />175,666</Col>
+          <Col span={5} className={styles.upload_amount}><img src="src/images/down_icon.svg" alt="" className={styles.down_icon} />2,077,777</Col>
+          <Col span={4} className={styles.time_component}>
+            <Time />
+          </Col>
+        </Row>
+        <Row className={styles.chart_component}>
+          <Col span={18} className={styles.chart_component_left}>
+            <Row className={styles.chart_component_left_header}>
+              <Col span={24} className={styles.chart_component_port}>
+                接口调用
+              </Col>
+            </Row>
+            <Row>
+              <Col span={12} className={styles.chart_component_data}>
+                数据处理图
+              </Col>
+              <Col span={12} className={styles.chart_component_server}>
+                服务器监控
+              </Col>
+            </Row>
+          </Col>
+          <Col span={6} className={styles.chart_component_real_time}>
+            实况数据
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+}
+
+App.propTypes = {
+}
---
+++ src/components/time/Time.jsx
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react'
+import { Row, Col, Icon, Button } from 'antd'
+import classnames from 'classnames';
+import styles from './Time.less';
+// import './Time.less'
+
+export class Time extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div className={styles.Time_content}>
+        <Row>
+          <Col span={12} className={styles.time_data}>
+            2016.09.07
+          </Col>
+          <Col span={12} className={styles.time_week} >
+            星期三
+          </Col>
+          <Col span={24} className={styles.time_clock}>
+            10:50:38
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+Time.defaultProps = {
+}
+
+Time.propTypes = {
+}
